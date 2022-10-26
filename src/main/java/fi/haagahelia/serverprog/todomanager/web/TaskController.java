2a69bebfce0d3c11b9634d62309bff0cef1cc823
MatthieuBruh <matthieu.bruhwiler@etu.hesge.ch>
2022-10-26 20:14:28 +0300

    Adding category sort on tasksList.html

--- src/main/java/fi/haagahelia/serverprog/todomanager/web/TaskController.java
+++ src/main/java/fi/haagahelia/serverprog/todomanager/web/TaskController.java
@@ -127,7 +127,8 @@ public class TaskController {
 
         List<Task> tasks = extractPersonTasks((List<Task>) trepository.findAll(), request.getUserPrincipal());
         if (categoryFilter != null) {
-            tasks.removeIf(task -> !task.getCategory().equals(categoryFilter));
+            // System.out.println("Filtering by category: " + categoryFilter.getTitle());
+            tasks.removeIf(task -> !task.getCategory().getTitle().equals(categoryFilter.getTitle()));
         }
         tasks.sort(new SortByDueDateAndPriority());
         model.addAttribute("tasks", tasks);
